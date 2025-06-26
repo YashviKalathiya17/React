94a50b6e572258742a67eb10ccb750add9a3d01f
YashviKalathiya17 <yashvikalathiya402@gmail.com>
2025-06-26 19:17:09 +0530

    Eventhandler

---
+++ Eventhandler/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useTheme } from './context/ThemeContext';
+
+function App() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h1>Current Theme: {theme.toUpperCase()}</h1>
+      <button
+        className={theme === 'light' ? 'light-button' : 'dark-button'}
+        onClick={toggleTheme}
+      >
+        Toggle to {theme === 'light' ? 'Dark' : 'Light'} Mode
+      </button>
+    </div>
+  );
+}
+
+export default App;
